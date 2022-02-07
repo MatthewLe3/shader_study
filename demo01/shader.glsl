2e639937318b1507df3ea9b8f8ed787b8353fd16
taoli <15230817603@163.com>
2022-02-07 15:46:00 +0800

    网格

--- demo01/shader.glsl
+++ demo01/shader.glsl
@@ -1,20 +1,25 @@
-vec3 NumberPlane(vec2 uv) {
+vec3 Grid(vec2 uv) {
     vec3 color = vec3(0.0);
+    vec2 fraction = 1.0 - 2.0 * abs(fract(uv) - 0.5);
 
-    vec2 ceil = fract(uv);
     // fwidth 单位像素宽度
-    if(ceil.x <= fwidth(uv.x)) {
-        color = vec3(1.0);
-    }
-    if(ceil.y <= fwidth(uv.y)) {
+    if(abs(uv.x) <= 2.0 * fwidth(uv.x)) {
+        color.g = 1.0;
+    } else if(abs(uv.y) <= 2.0 * fwidth(uv.y)) {
+        color.r = 1.0;
+    } else if(fraction.x < 2.0 * fwidth(uv.x) || fraction.y < 2.0 * fwidth(uv.y)) {
         color = vec3(1.0);
     }
 
     return color;
 }
 
-void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 uv = 3.0 * (2.0 * fragCoord - iResolution.xy) / min(iResolution.x, iResolution.y);
+vec2 fixUV(in vec2 c) {
+    return 3.0 * (2.0 * c - iResolution.xy) / min(iResolution.x, iResolution.y);
+}
 
-    fragColor = vec4(NumberPlane(uv), 1.0);
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec2 uv = fixUV(fragCoord);
+
+    fragColor = vec4(Grid(uv), 1.0);
 }
